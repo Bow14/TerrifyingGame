655a23d045f9780ab943639c2aadd3589f88477b
TkBow <thebow25@gmail.com>
2023-01-20 18:41:10 -0700

    Camera rotation is working

--- Terror/Assets/CameraControls.cs
+++ Terror/Assets/CameraControls.cs
@@ -7,6 +7,14 @@ public class CameraControls : MonoBehaviour
     public Joystick stick;
     public float horizontalInput = Input.GetAxis("Horizontal");
     public float verticalInput = Input.GetAxis("Vertical");
+
+    public Rigidbody camera;
+    public float speed;
+    public GameObject cameraObject;
+
+    private Vector3 offset = new Vector3(0, 2, 0);
+
+    public GameObject player;
     //public Vector3 stickControls = new Vector3(horizontalInput,0 verticalInput);
     
 
@@ -21,10 +29,14 @@ public class CameraControls : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-    
-        
+
+        camera.rotation = camera.rotation * Quaternion.AngleAxis(stick.Vertical * speed, Vector3.down);
+        //camera.rotation = camera.rotation * Quaternion.AngleAxis(stick.Horizontal * speed, Vector3.right);
+        //cameraObject.transform.rotation;
+
+        transform.position = player.transform.position + offset;
 
 
-}
+    }
 
 }
