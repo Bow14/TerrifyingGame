fb27aeea867683f2cf9bc1178033ee34cfffe9db
TkBow <thebow25@gmail.com>
2023-02-10 14:32:36 -0700

    Added interface and such for coins
    A

---
+++ Terror/Assets/Interface.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickup
+{
+  public void Collect(MoveScript playerObj);
+
+}
--- Terror/Assets/OnCollisonBehaviour.cs
+++ Terror/Assets/OnCollisonBehaviour.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class OnCollisonBehaviour : MonoBehaviour
+public class OnCollisonBehaviour : MonoBehaviour,IPickup
 {
     public UnityEvent collisionEvent, updateEvent;
+    public MoveScript playerObj;
     private void OnCollisionEnter(Collision collision)
     {
         collisionEvent.Invoke();
+        Collect(playerObj);
+    }
+
+    public void Collect(MoveScript player)
+    {
         updateEvent.Invoke();
+
     }
 }
